4dab8a56ab47a6f5150a551d1006fe20feaf29bf
maaaashin324 <mstk.shin@gmail.com>
2018-02-23 11:30:04 +0900

    set up knexfile, make migration files, and set up yarn command for migaration and rollback. As to flight plan table, we need to add columns after figuring out how to use API.

--- server/services/db/knexfile.js
+++ server/services/db/knexfile.js
@@ -1,3 +1,10 @@
 module.exports = {
-  
+  client: 'pg',
+  connection: {
+    host: process.env.DATABASE_HOST || '127.0.0.1',
+    database: 'rescue_drone',
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+  },
 };
