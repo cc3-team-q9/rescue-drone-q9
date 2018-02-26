93384132c7f9bddd8a7ebe13c209b6005c50fc3e
maaaashin324 <mstk.shin@gmail.com>
2018-02-26 09:08:44 +0900

    modify knexfile in order to fix config of pg

--- middlewares/db/knexfile.js
+++ middlewares/db/knexfile.js
@@ -1,7 +1,8 @@
 module.exports = {
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_HOST || '127.0.0.1',
+    host: process.env.DATABASE_URL || '127.0.0.1',
+    port: process.env.DATABASE_PORT || 5432,
     database: 'rescue_drone',
   },
   migrations: {
