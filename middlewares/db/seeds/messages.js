01acf3f09b7f278094480ef05eb49073e6be0237
maaaashin324 <mstk.shin@gmail.com>
2018-02-27 17:22:13 +0900

    fix lng and lat

--- middlewares/db/seeds/messages.js
+++ middlewares/db/seeds/messages.js
@@ -7,26 +7,26 @@ exports.seed = (knex, Promise) =>
         {
           username: 'Alice',
           message: 'Avalanche happends. Help me.',
-          longitude: 36.7477191,
-          latitude: 138.128287,
+          latitude: 36.7477191,
+          longitude: 138.128287,
         },
         {
           username: 'Bob',
           message: 'Volcano erupts. Help me.',
-          longitude: 36.1695501,
-          latitude: 137.4549663,
+          latitude: 36.1695501,
+          longitude: 137.4549663,
         },
         {
           username: 'Satoshi',
           message: 'Huge earthquake happends. Help me.',
-          longitude: 35.726134,
-          latitude: 136.3496945,
+          latitude: 35.726134,
+          longitude: 136.3496945,
         },
         {
           username: 'Sephiroth',
           message: 'Reunion. Fire.',
-          longitude: 35.0736761,
-          latitude: 134.6303915,
+          latitude: 35.0736761,
+          longitude: 134.6303915,
         },
       ]));
 
